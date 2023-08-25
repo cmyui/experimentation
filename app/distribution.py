ceb791c98d9fe4c989c532bc21a3544765ce2b4b
cmyui <cmyuiosu@gmail.com>
2023-08-25 01:19:50 -0400

    remove outdated division

--- app/distribution.py
+++ app/distribution.py
@@ -11,14 +11,10 @@ def get_user_variant(experiment: Experiment, user_id: str) -> str:
     identifier = f"{experiment.bucketing_salt}:{user_id}"
     normalized_value = normalize_value(identifier)
 
-    bucket_weights = {
-        variant_name: weight / 100  # 53% -> 0.53
-        for variant_name, weight in experiment.variant_allocation.items()
-    }
     cumulative_weight = 0.0
-    for bucket_name, weight in bucket_weights.items():
+    for variant_name, weight in experiment.variant_allocation.items():
         cumulative_weight += weight
         if normalized_value < cumulative_weight:
-            return bucket_name
+            return variant_name
 
     raise ValueError("Bucket weights do not sum to 1.0")
